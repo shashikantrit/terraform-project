389b7583635b6a45b1847929040cdc15b9a90f66
shashikantrit <51086818+shashikantrit@users.noreply.github.com>
2024-06-07 23:07:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 0.14.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "environments" {
+  source = "./modules/object"
+
+  for_each = var.environments
+
+  environment = each.key
+  config      = each.value
+}
